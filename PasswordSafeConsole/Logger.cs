3b049168c3fa4e08a7b8c07865506b8ed01d8010
Maximilian Turza <maxturza@hotmail.com>
2022-10-24 18:54:51 +0200

    added Logger

---
+++ PasswordSafeConsole/Logger.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+namespace PasswordSafeConsole
+{
+
+    public class Logger
+    {
+        public enum Type
+        {
+            INFO,
+            ERROR,
+            DEBUG
+        }
+        public static void WriteLog(string message, Type type)
+        {
+            Console.WriteLine($"[{type}] : {message}");
+
+            string logPath = "/logs";
+
+            // Preparation for logging into files
+            using (StreamWriter write = new StreamWriter(logPath, true))
+            {
+                write.WriteLine(message);
+            }
+
+        }
+
+
+    }
+}
--- PasswordSafeConsole/Program.cs
+++ PasswordSafeConsole/Program.cs
@@ -15,7 +15,7 @@ namespace PasswordSafeConsole
 
             bool abort = false;
             bool unlocked = false;
-            while (!abort) 
+            while (!abort)
             {
                 Console.WriteLine("Enter master (1), show all (2), show single (3), add (4), delete(5), set new master (6), Abort (0)");
                 int input = 0;
@@ -23,116 +23,164 @@ namespace PasswordSafeConsole
                 {
                     input = -1;
                 }
-                switch (input) 
+                switch (input)
                 {
-                     case 0: 
-                     {
-                        abort = true;
-                        break;
-                     }
-                     case 1: 
-                     {
-                        Console.WriteLine("Enter master password");
-                        String masterPw = Console.ReadLine();
+                    case 0:
+                        {
+                            abort = true;
+                            break;
+                        }
+                    case 1:
+                        {
+                            Console.WriteLine("Enter master password");
+                            String masterPw = Console.ReadLine();
 
-                        unlocked = masterRepository.MasterPasswordIsEqualTo(masterPw);
-                        if (unlocked) 
-                        {
-                            passwordSafeEngine = new PasswordSafeEngine("./passwords.pw", new CipherFacility(masterPw));
-                            Console.WriteLine("unlocked");
-                        } else
-                        {
-                            Console.WriteLine("master password did not match ! Failed to unlock.");
-                        }
-                        break;
-                     }
-                     case 2: 
-                     {
-                        if (unlocked)
-                        {
-                            passwordSafeEngine.GetStoredPasswords().ToList().ForEach(pw=>Console.WriteLine(pw));
-                        }
-                        else
-                        {
-                            Console.WriteLine("Please unlock first by entering the master password.");
-                        }
-                        break;
-                    }
-                    case 3: {
-                        if (unlocked)
-                        {
-                            Console.WriteLine("Enter password name");
-                            String passwordName = Console.ReadLine();
-                            Console.WriteLine(passwordSafeEngine.GetPassword(passwordName));
-                        }
-                        else
-                        {
-                            Console.WriteLine("Please unlock first by entering the master password.");
-                        }
-                        break;
-                    }
-                    case 4: 
-                    {
-                        if (unlocked)
-                        {
-                            Console.WriteLine("Enter new name of password");
-                            String passwordName = Console.ReadLine();
-                            Console.WriteLine("Enter password");
-                            String password = Console.ReadLine();
-                            passwordSafeEngine.AddNewPassword(new PasswordInfo(password, passwordName));
-                        }
-                        else
-                        {
-                            Console.WriteLine("Please unlock first by entering the master password.");
-                        }
-                        break;
-                    }
-                    case 5: 
-                    {
-                        if (unlocked)
-                        {
-                            Console.WriteLine("Enter password name");
-                            String passwordName = Console.ReadLine();
-                            passwordSafeEngine.DeletePassword(passwordName);
-                        }
-                        else
-                        {
-                            Console.WriteLine("Please unlock first by entering the master password.");
-                        }
-                        break;
-                    }
-                    case 6:
-                    {
-                        unlocked = false;
-                        passwordSafeEngine = null;
-                        string masterPw;
-                        string confirmPw;
-                        do
-                        {
-                            Console.WriteLine("Enter new master password ! (Warning you will loose all already stored passwords)");
-                            masterPw = Console.ReadLine();
-                            Console.WriteLine("Please confirm master password ! ");
-                            confirmPw = Console.ReadLine();
-                            if (masterPw != confirmPw)
+                            unlocked = masterRepository.MasterPasswordIsEqualTo(masterPw);
+                            if (unlocked)
                             {
-                                Console.WriteLine("Passwords do not match! Try again! ");
-                            }
-                        } while (masterPw != confirmPw);
+                                try
+                                {
+                                    passwordSafeEngine = new PasswordSafeEngine("./passwords.pw", new CipherFacility(masterPw));
+                                    // Console.WriteLine("unlocked");
+                                    Logger.WriteLog("unlocked", Logger.Type.INFO);
 
-                        masterRepository.SetMasterPasswordPlain(masterPw);
-                        // urgent hotfix delete old passwords after changing the master
-                        if (Directory.Exists("./passwords.pw"))
-                        {
-                            Directory.Delete("./passwords.pw", true);
+                                }
+                                catch (Exception ex)
+                                {
+                                    Logger.WriteLog(ex.ToString(), Logger.Type.ERROR);
+                                }
+                            }
+                            else
+                            {
+                                // Console.WriteLine("master password did not match ! Failed to unlock.");
+                                Logger.WriteLog("master password did not match ! Failed to unlock.", Logger.Type.ERROR);
+                            }
+                            break;
                         }
-                        break;
-                    }
+                    case 2:
+                        {
+                            if (unlocked)
+                            {
+                                try
+                                {
+                                    passwordSafeEngine.GetStoredPasswords().ToList().ForEach(pw => Console.WriteLine(pw));
+                                }
+                                catch (Exception ex)
+                                {
+                                    Logger.WriteLog(ex.ToString(), Logger.Type.ERROR);
+                                }
+                            }
+                            else
+                            {
+                                Logger.WriteLog("Please unlock first by entering the master password.", Logger.Type.ERROR);
+                            }
+                            break;
+                        }
+                    case 3:
+                        {
+                            if (unlocked)
+                            {
+                                Console.WriteLine("Enter password name");
+                                String passwordName = Console.ReadLine();
+                                Console.WriteLine(passwordSafeEngine.GetPassword(passwordName));
+                            }
+                            else
+                            {
+                                Logger.WriteLog("Please unlock first by entering the master password.", Logger.Type.ERROR);
+                            }
+                            break;
+                        }
+                    case 4:
+                        {
+                            if (unlocked)
+                            {
+                                Console.WriteLine("Enter new name of password");
+                                String passwordName = Console.ReadLine();
+                                Console.WriteLine("Enter password");
+                                String password = Console.ReadLine();
+                                try
+                                {
+                                    passwordSafeEngine.AddNewPassword(new PasswordInfo(password, passwordName));
+                                    Logger.WriteLog("New Password added", Logger.Type.INFO);
+                                }
+                                catch (Exception ex)
+                                {
+                                    Logger.WriteLog(ex.ToString(), Logger.Type.ERROR);
+                                }
+                            }
+                            else
+                            {
+                                Logger.WriteLog("Please unlock first by entering the master password.", Logger.Type.ERROR);
+                            }
+                            break;
+                        }
+                    case 5:
+                        {
+                            if (unlocked)
+                            {
+                                Console.WriteLine("Enter password name");
+                                String passwordName = Console.ReadLine();
+                                try
+                                {
+                                    passwordSafeEngine.DeletePassword(passwordName);
+                                    Logger.WriteLog("Password removed", Logger.Type.INFO);
+
+                                }
+                                catch (Exception ex)
+                                {
+                                    Logger.WriteLog(ex.ToString(), Logger.Type.ERROR);
+
+                                }
+                            }
+                            else
+                            {
+                                Logger.WriteLog("Please unlock first by entering the master password.", Logger.Type.ERROR);
+                            }
+                            break;
+                        }
+                    case 6:
+                        {
+                            unlocked = false;
+                            passwordSafeEngine = null;
+                            string masterPw;
+                            string confirmPw;
+                            do // Added confirmation
+                            {
+                                Console.WriteLine("Enter new master password ! (Warning you will loose all already stored passwords)");
+                                masterPw = Console.ReadLine();
+                                Console.WriteLine("Please confirm master password ! ");
+                                confirmPw = Console.ReadLine();
+                                if (masterPw != confirmPw)
+                                {
+                                    // Console.WriteLine("Passwords do not match! Try again! ");
+                                    Logger.WriteLog("Passwords do not match! Try again! ", Logger.Type.ERROR);
+                                }
+                            } while (masterPw != confirmPw);
+
+                            masterRepository.SetMasterPasswordPlain(masterPw);
+                            // urgent hotfix delete old passwords after changing the master
+                            if (Directory.Exists("./passwords.pw"))
+                            {
+                                try
+                                {
+                                    Directory.Delete("./passwords.pw", true);
+                                    Logger.WriteLog("successfully deleted", Logger.Type.INFO);
+
+                                }
+                                catch (Exception ex)
+                                {
+                                    Logger.WriteLog(ex.ToString(), Logger.Type.ERROR);
+                                }
+
+                            }
+                            break;
+                        }
                     default:
-                    {
-                        Console.WriteLine("Invalid input");
-                        break;
-                    }
-                       
+                        {
+                            Logger.WriteLog("Invalid input", Logger.Type.ERROR);
+                            break;
+                        }
+
                 }
             }
 
